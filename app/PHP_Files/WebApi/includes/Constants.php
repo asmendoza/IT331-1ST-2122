e971eda31e5119bc23e952588399f96ca54f9e1e
Aris Gail Mendoza <arisgailseemendoza@gmail.com>
2021-10-27 03:41:01 +0800

    - Added the PHP files for connecting to MySQL database (for uploading to web server) - If you want to try the app,   1. edit the Constants.php to connect to your assigned database   2. edit the EndPoints.kt to put the group number

---
+++ app/PHP_Files/WebApi/includes/Constants.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_HOST','<serverName>');
+    define('DB_USERNAME','<MySQL_Username>');
+    define('DB_PASSWORD','<MySQL_Password>');
+    define('DB_NAME', '<MySQL_Database>');
+?>
--- app/src/main/java/com/batstateu/it331_1st_2122/EndPoints.kt
+++ app/src/main/java/com/batstateu/it331_1st_2122/EndPoints.kt
@@ -1,8 +1,10 @@
 package com.batstateu.it331_1st_2122
 
 object EndPoints {
-    private const val URL_ROOT = "https://aris-gail.com/WebApi/v1/?op="
-    //private const val URL_ROOT = "https://developers.aris-gail.com/21221ST_CS3101_G1/WebApi/v1/?op="
+    //private const val URL_ROOT = "https://aris-gail.com/WebApi/v1/?op="
+    private const val groupNo: Int = 0 //your group number here
+    private const val URL_ROOT =
+        "https://developers.aris-gail.com/21221ST_CS3101_G$groupNo/WebApi/v1/?op="
     val URL_ADD_ARTIST = URL_ROOT + "addartist"
     val URL_GET_ARTIST = URL_ROOT + "getartists"
 }
